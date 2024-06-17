91c1842e48aed1998c79c33c025a7d3ddf54a121
hardik8127 <hardikkumar159515@gmail.com>
2024-06-17 16:40:36 +0530

    BST Implementation

---
+++ binarySearchTree/bst.js
@@ -0,0 +1,17 @@
+class BSTNode{
+    constructor (key){
+        this.key = key;
+        this.left = null
+        this.right = null
+    }
+}
+// Designing or Implementaion of Binary Search Tree
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+}
+
+// Insertion in Binary Search Tree
+
